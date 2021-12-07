5c0fa70326522efc1f75e9ea89980e48663a4147
amalalqoud <AmalAlqoud@live.com>
2021-12-08 00:23:13 +0300

    barCharts

--- js/charts.js
+++ js/charts.js
@@ -3,7 +3,74 @@
 var ordersChart = document.getElementById("myAreaChart");
 
 // Bar Chart for Visitor
-var visitorChart = document.getElementById("myBarChart");
+//var visitorChart = document.getElementById("myBarChart");
+    var ctx = document.getElementById("myBarChart");
+                                var myChart = new Chart(ctx, {
+                                    type: 'bar',
+                                    data: {
+                                        labels: ["June", "July", "August", "September", "October", "November", "December"],
+                                        datasets: [
+                                            {
+                                                label: 'Men',
+                                                data: [3000, 4100, 13000, 5000, 8000, 9000, 13000],
+                                                backgroundColor: ['rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)'
+                                                ],
+
+                                                borderColor: [
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)',
+                                                    'rgba(255,99,132,1)'
+                                                ],
+                                                borderWidth: 1
+                                            },
+                                            {
+                                                label: 'Women',
+                                                data: [5100, 5140, 15000, 6200, 6000, 8000, 10000],
+                                                backgroundColor: ['rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)'
+                                                ],
+
+                                                borderColor: [
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(54, 162, 235, 1)'
+                                                ],
+                                                borderWidth: 1
+                                            }
+                                        ]
+                                    },
+                                    options: {
+                                        scales: {
+                                            yAxes: [{
+                                                ticks: {
+                                                    beginAtZero: true
+                                                }
+                                            }]
+                                        }
+                                    }
+                                });
+
 
 // Liner Chart for Cities
 var citiesChart = document.getElementById("myPieChart");
