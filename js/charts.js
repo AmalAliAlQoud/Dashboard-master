89aab5110b25bd34a6c3397556c33d9424985c64
amalalqoud <AmalAlqoud@live.com>
2021-12-08 23:47:24 +0300

    allCharts

--- js/charts.js
+++ js/charts.js
@@ -93,7 +93,61 @@ var myChart = new Chart(ordersChart, {
 
 
 // Liner Chart for Cities
+//var citiesChart = document.getElementById("myPieChart");
 var citiesChart = document.getElementById("myPieChart");
 
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ["React", "Angular", "Vue", "Hyperapp", "Omi"];
+
+var myChart = new Chart(citiesChart, {
+  type: "pie",
+  data: {
+    labels: frameworks,
+    datasets: [
+      {
+        label: "Github Stars",
+        data: stars,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+});
+
 // Liner Chart for Rat
-var ratChart = document.getElementById("myLinearChart");
+//var ratChart = document.getElementById("myLinearChart");
+var ratChart = document.getElementById("myLinerChart");
+
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ["React", "Angular", "Vue", "Hyperapp", "Omi"];
+
+var myChart = new Chart(ratChart, {
+  type: "line",
+  data: {
+    labels: frameworks,
+    datasets: [
+      {
+        label: "Github Stars",
+        data: stars,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+        fill: false,
+        lineTension: 0 
+      }
+    ]
+  }
+});
